cf5972bf444ab6625e9fcfd07f8cb2a1ef54894d
leewoorim <lwr0221716@naver.com>
2023-03-02 20:16:17 +0900

    chapter 2 update

---
+++ lib/api/api.dart
@@ -0,0 +1,7 @@
+class API {
+  static const hostConnect = "http://192.168.1.123/api_test1";
+  static const hostConnectUser = "$hostConnect/user";
+
+  static const signup = "$hostConnect/user/signup.php";
+  static const validataEmail = "$hostConnect/user/validate_email.php";
+}
